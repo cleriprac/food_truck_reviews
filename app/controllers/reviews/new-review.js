e134490a45bed8c9a06064c7b602fedf58a58af3
Guest User <student@epicodus.com>
2015-06-17 16:00:34 -0700

    associate review with a particular truck in controller

--- app/controllers/reviews/new-review.js
+++ app/controllers/reviews/new-review.js
@@ -1,13 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  needs: ['truck'],
   actions: {
     addReview: function() {
       var newReview = this.store.createRecord('review', {
         author: this.get('author'),
-        review: this.get('review'),
+        review: this.get('review')
       });
       newReview.save();
+
+      var truck = this.get('controllers.truck.model');
+      truck.get('reviews').pushObject(newReview);
+      truck.save();
+
       this.setProperties({
         author: '',
         review: ''
